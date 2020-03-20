3be2343968d80d863e764e00e38ef350b1115030
Igor Zhivilo <warolv@gmail.com>
2020-03-21 00:04:23 +0200

    rename terraform folder to tf

--- .travis/deploy_ec2.sh
+++ .travis/deploy_ec2.sh
@@ -5,6 +5,6 @@ ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/ec2_deploy_key -q -N ""
 export TF_VAR_public_deploy_key=$HOME/.ssh/ec2_deploy_key.pub
 export TF_VAR_private_deploy_key=$HOME/.ssh/ec2_deploy_key
 
-cd ../terraform
+cd ../tf
 terraform init $tf_init_cli_options
 terraform apply $tf_init_cli_options
