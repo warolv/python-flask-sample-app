6e976589bc5b5589d45a2da9824da7af19277cd3
Igor Zhivilo <warolv@gmail.com>
2020-03-20 23:41:17 +0200

    Changes to deploy_ec2.sh script

--- .travis/deploy_ec2.sh
+++ .travis/deploy_ec2.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/ec2_deploy_key -q -N ""
+
+export TF_VAR_public_deploy_key=$HOME/.ssh/ec2_deploy_key.pub
+export TF_VAR_private_deploy_key=$HOME/.ssh/ec2_deploy_key
+
 cd ../terraform
 terraform init $tf_init_cli_options
 terraform apply $tf_init_cli_options
